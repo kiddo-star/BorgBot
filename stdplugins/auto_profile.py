60758aacccc9d1e372e9556ec70b52a99e60d559
kiddo-star <54259130+kiddo-star@users.noreply.github.com>
2019-08-30 22:59:43 +0530

    Update auto_profile.py

--- stdplugins/auto_profile.py
+++ stdplugins/auto_profile.py
@@ -20,7 +20,7 @@ async def _(event):
     while True:
         DMY = time.strftime("%d.%m.%Y")
         HM = time.strftime("%H:%M:%S")
-        bio = f"📅 {DMY} | KNOWLEDGE HAS A PRICE | ⌚️ {HM}"
+        bio = f"KNOWLEDGE HAS A PRICE | 📅 {DMY} | ⌚️ {HM}"
         logger.info(bio)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
@@ -45,7 +45,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d.%m.%y")
         HM = time.strftime("%H:%M")
-        name = f"⌚{HM}|Λгɳαɓ|📅{DM}"
+        name = f"⌚{HM}|Kiddo|📅{DM}"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
